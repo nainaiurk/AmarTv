497cc193d935c8f9b07735dbb5da96b4761e3b65
nainaiurk <nainaiu.rakhaine@gmail.com>
2022-11-24 22:57:23 +0600

    hide cast

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1155,65 +1155,68 @@ class _MyHomePageState extends State<MyHomePage> {
               Positioned(
                 bottom: 30,
                 right: 30,
-                child: FloatingActionButton(
-                  backgroundColor: const Color.fromARGB(255, 179, 17, 6),
-                  elevation: 10,
-                  onPressed: (){
-                    _startSearch();
-                    showDialog(
-                      context: context, 
-                      builder: (BuildContext context)=>
-                        AlertDialog(    
-                          scrollable: true,    
-                          content: FutureBuilder<List<CastDevice>>(
-                            future: _future,
-                            builder: (context, snapshot) {
-                              if (snapshot.hasError) {
-                                return Center(
-                                  child: Text(
-                                    'Error: ${snapshot.error.toString()}',
-                                  ),
-                                );
-                              } 
-                              else if (!snapshot.hasData) {
-                                return const Center(
-                                  child: CircularProgressIndicator(),
-                                );
-                              }
-                
-                              if (snapshot.data.isEmpty) {
-                                return Column(
-                                  children: const [
-                                    Center(
-                                      child: Text(
-                                        'No Chromecast founded',
-                                      ),
+                child: Visibility(
+                  visible: false,
+                  child: FloatingActionButton(
+                    backgroundColor: const Color.fromARGB(255, 179, 17, 6),
+                    elevation: 10,
+                    onPressed: (){
+                      _startSearch();
+                      showDialog(
+                        context: context, 
+                        builder: (BuildContext context)=>
+                          AlertDialog(    
+                            scrollable: true,    
+                            content: FutureBuilder<List<CastDevice>>(
+                              future: _future,
+                              builder: (context, snapshot) {
+                                if (snapshot.hasError) {
+                                  return Center(
+                                    child: Text(
+                                      'Error: ${snapshot.error.toString()}',
                                     ),
-                                  ],
-                                );
-                              }
-                
-                              return Column(
-                                children: snapshot.data.map((device) {
-                                  return ListTile(
-                                    title: Text(device.name),
-                                    onTap: () {
-                                      _connectToYourApp(context, device);
-                                      _connectAndPlayMedia(context, device);
-                                    },
                                   );
-                                }).toList(),
-                              );
-                            },
-                          ),
-                        )
-                    );
-                  },
-                  child: const Icon(
-                    Icons.cast,
-                    color: Colors.white,
-                  )
-                  ),
+                                } 
+                                else if (!snapshot.hasData) {
+                                  return const Center(
+                                    child: CircularProgressIndicator(),
+                                  );
+                                }
+                  
+                                if (snapshot.data.isEmpty) {
+                                  return Column(
+                                    children: const [
+                                      Center(
+                                        child: Text(
+                                          'No Chromecast founded',
+                                        ),
+                                      ),
+                                    ],
+                                  );
+                                }
+                  
+                                return Column(
+                                  children: snapshot.data.map((device) {
+                                    return ListTile(
+                                      title: Text(device.name),
+                                      onTap: () {
+                                        _connectToYourApp(context, device);
+                                        _connectAndPlayMedia(context, device);
+                                      },
+                                    );
+                                  }).toList(),
+                                );
+                              },
+                            ),
+                          )
+                      );
+                    },
+                    child: const Icon(
+                      Icons.cast,
+                      color: Colors.white,
+                    )
+                    ),
+                ),
               ),
             ],
           ),
